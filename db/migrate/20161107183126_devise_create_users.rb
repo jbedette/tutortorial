a06a10f7ab946df9003f8b214fc29160467a5b7d
Arnold Hundert <hunderta@yahoo.com>
2016-11-07 16:08:49 -0500

    Added fields to User migration file

--- db/migrate/20161107183126_devise_create_users.rb
+++ db/migrate/20161107183126_devise_create_users.rb
@@ -2,6 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :fname, limit: 20
+      t.string :lname, limit: 20
+      t.string :username, limit: 20
+      t.string :usertype, limit: 7
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
