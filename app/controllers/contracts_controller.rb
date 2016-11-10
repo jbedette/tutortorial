917443168ca44fb1537e59f793b8e588c07d1dc4
jbedette <jbedette@yahoo.com>
2016-11-10 17:11:36 -0500

    email stuff

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -3,5 +3,7 @@ class ContractsController < ApplicationController
   def index
     @message ="Tutortorial Contract Page"
   end
-
+  def email_send
+    UserMailer.contact_email(@user).deliver_later
+  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from 'partyville@usa.com'
+
+  def contact_email(user)
+    @user = user
+    @url = 'localhost:3000'
+    mail(to: @user.email, subject: 'Your Tutor!')
+  end
 end
